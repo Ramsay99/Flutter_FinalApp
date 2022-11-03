19a47dccef68aaa2211201b154d7c1b05e03f1ee
Ramzi Sayegh <sayegh.ramzi@yahoo.com>
2022-11-03 19:54:51 +0200

    Update screens_barrel.dart

--- finalapp/lib/Screens/screens_barrel.dart
+++ finalapp/lib/Screens/screens_barrel.dart
@@ -1,1 +1,9 @@
+export 'package:flutter/material.dart';
 export 'package:finalapp/Screens/login_screen.dart';
+export 'package:finalapp/Screens/employees_screen.dart';
+export 'package:finalapp/Screens/home_screen.dart';
+export 'package:finalapp/Screens/dashboard_screen.dart';
+export 'package:finalapp/Screens/history_screen.dart';
+
+
+// export 'package:finalapp/Screens/.dart';
