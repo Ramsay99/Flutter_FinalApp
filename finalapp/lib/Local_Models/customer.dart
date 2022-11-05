d54919afcb28ec7cb527a401fd56e00ee586e1fa
Ramzi Sayegh <sayegh.ramzi@yahoo.com>
2022-11-05 17:42:45 +0200

    add attributes & const in Customer

--- finalapp/lib/Local_Models/customer.dart
+++ finalapp/lib/Local_Models/customer.dart
@@ -2,9 +2,18 @@ import 'package:finalapp/Local_Models/user.dart';
 import '../screens/screens_barrel.dart';
 
 class Customer extends User {
-  Customer() {
+  late final String _name;
+  late int _phoneNum;
+
+  Customer({
+    required String name,
+    required int phoneNum,
+  }) {
+    _name = name;
+    _phoneNum = phoneNum;
     print("New Customer!");
   }
+  
   @override
   Iterable<Widget> getAvailableScreens() {
     return [];
