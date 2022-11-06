1fe84e7a3658ef973210030ab44c404a071bd955
Ramzi Sayegh <sayegh.ramzi@yahoo.com>
2022-11-06 14:23:54 +0200

    create animated Star Rating
    not implemented yet

---
+++ finalapp/lib/Screens/customer_screens/rating_star/rating_star.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:graphx/graphx.dart';
+
+import 'scene/rating_scene.dart';
+
+class RatingStarsMain extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('rating stars')),
+      body: SceneBuilderWidget(
+        builder: () => SceneController(back: RatingStarsScene()),
+        autoSize: true,
+      ),
+    );
+  }
+}
