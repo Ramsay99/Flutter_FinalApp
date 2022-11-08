5ffb89cd2a9d30a200f64906f633a890284c6cf6
Ramzi Sayegh <sayegh.ramzi@yahoo.com>
2022-11-08 08:02:41 +0200

    Update map_vis.dart

--- finalapp/lib/Screens/manager_screens/dashboard/map_vis.dart
+++ finalapp/lib/Screens/manager_screens/dashboard/map_vis.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
-
 import 'package:flutter/material.dart';
 
 class MapVis extends StatefulWidget {
@@ -10,41 +8,89 @@ class MapVis extends StatefulWidget {
 }
 
 class _MapVisState extends State<MapVis> {
-  Image? _jordanMap;
+  Image? _jordanMapImage;
+  late String _irbidMapImage;
+  late String _ajlunMapImage;
+  late String _jarashMapImage;
+  late String _mafraqMapImage;
+  late String _saltMapImage;
+  late String _zarqaMapImage;
+  late String _madabaMapImage;
+  late String _ammanMapImage;
+  late String _karakMapImage;
+  late String _tafelaMapImage;
+  late String _maanMapImage;
+  late String _aqabaMapImage;
+
   @override
   void initState() {
     super.initState();
-    _jordanMap = Image.asset('assets/images/jordanMap.png');
+    _jordanMapImage = Image.asset('assets/images/jordanMap.png');
+    _irbidMapImage = 'assets/images/irbid.png';
+    _ajlunMapImage = 'assets/images/ajlun.png';
+    _jarashMapImage = 'assets/images/jarash.png';
+    _mafraqMapImage = 'assets/images/mafraq.png';
+    _saltMapImage = 'assets/images/salt.png';
+    _zarqaMapImage = 'assets/images/zarqa.png';
+    _madabaMapImage = 'assets/images/madaba.png';
+    _ammanMapImage = 'assets/images/amman.png';
+    _karakMapImage = 'assets/images/karak.png';
+    _tafelaMapImage = 'assets/images/tafela.png';
+    _maanMapImage = 'assets/images/maan.png';
+    _aqabaMapImage = 'assets/images/aqaba.png';
   }
 
   @override
   Widget build(BuildContext context) {
+    // Vals will be taken from FireStore `Cities` :P
+    int testColIncrementaly = 0;
+    int mapCol() {
+      testColIncrementaly += (256 / 12).round();
+      print(testColIncrementaly);
+      return testColIncrementaly;
+    }
+
     return Scaffold(
-      appBar: AppBar(title: Text("Map")),
-      body: Stack(
-        children: [
-          _jordanMap!,
-          _City('assets/images/madaba.png', 33),
-          _City('assets/images/ajlun.png', 122),
-          _City('assets/images/amman.png', 111),
-          _City('assets/images/aqaba.png', 118),
-          _City('assets/images/irbid.png', 72),
-          _City('assets/images/jarash.png', 144),
-          _City('assets/images/karak.png', 166),
-          _City('assets/images/maan.png', 81),
-          _City('assets/images/mafraq.png', 99),
-          _City('assets/images/salt.png', 138),
-          _City('assets/images/tafela.png', 133),
-          _City('assets/images/zarqa.png', 166),
-        ],
+      appBar: AppBar(title: const Text("Map Geo Graph")),
+      body: Center(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height / 1.25,
+          child: _Map(mapCol),
+        ),
       ),
     );
   }
 
-  Image _City(String cityImgPath, int density) {
+  Stack _Map(int mapCol()) {
+    return Stack(
+      children: [
+        _jordanMapImage!,
+        _defaultCity(_ajlunMapImage, mapCol()),
+        _defaultCity(_irbidMapImage, mapCol()),
+        _defaultCity(_jarashMapImage, mapCol()),
+        _defaultCity(_mafraqMapImage, mapCol()),
+        _defaultCity(_saltMapImage, mapCol()),
+        _defaultCity(_zarqaMapImage, mapCol()),
+        _defaultCity(_madabaMapImage, mapCol()),
+        _defaultCity(_ammanMapImage, mapCol()),
+        _defaultCity(_karakMapImage, mapCol()),
+        _defaultCity(_tafelaMapImage, mapCol()),
+        _defaultCity(_maanMapImage, mapCol()),
+        _defaultCity(_aqabaMapImage, mapCol()),
+      ],
+    );
+  }
+
+  Widget _defaultCity(String cityImgPath, int density) {
+    int mapAlpha = density;
+    if (density < 30) {
+      mapAlpha = 30;
+    } else if (density > 160) {
+      mapAlpha = 170;
+    }
     return Image.asset(
       cityImgPath,
-      color: Color.fromARGB(255, density, 0, 0),
+      color: Color.fromARGB(mapAlpha, density, 0, 0),
     );
   }
 }
